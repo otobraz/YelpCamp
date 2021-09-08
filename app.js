8696309d245ef56e1ee93cfe41f1acaa20df6791
Oto Braz <oto.braz@outlook.com>
2021-09-08 16:22:53 -0300

    add start script

--- app.js
+++ app.js
@@ -170,4 +170,5 @@ app.use((err, req, res, next) => {
    res.status(status).render('error', { err });
 });
 
-app.listen(3000, () => console.log('SERVING ON PORT 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`SERVING ON PORT ${port}`));
